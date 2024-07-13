2d3756a057442ef055394fcd731f952aec8d25ae
ashimroyrupok <ashimroyrupok@gmail.com>
2024-07-13 23:47:21 +0600

    fix issue

--- Fitness-Equipment-and-Accessories-server/src/modules/products/product.services.ts
+++ Fitness-Equipment-and-Accessories-server/src/modules/products/product.services.ts
@@ -38,12 +38,9 @@ const getSingleProductFromDb = async (id: any) => {
   return result;
 };
 
-<<<<<<< HEAD
-const deleteProductFromDb = async (id: any) => {
-=======
+// const deleteProductFromDb = async (id: any) => {
 const deleteProductFromDb = async (id: string) => {
   console.log(id,"serve")
->>>>>>> 06a8b466ed13f798c0dab797df3533e09202c9dd
   const result = await Product.findByIdAndDelete(id);
   return result;
 };
