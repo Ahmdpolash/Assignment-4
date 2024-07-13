397b032f75d6374c4106fe9b34afb46ccbd82141
ashimroyrupok <ashimroyrupok@gmail.com>
2024-07-13 19:56:06 +0600

    some change in backend

--- Fitness-Equipment-and-Accessories-server/src/app.ts
+++ Fitness-Equipment-and-Accessories-server/src/app.ts
@@ -12,7 +12,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://fitness-equipment-and-accessories-client.vercel.app/",
+    ],
     credentials: true,
   })
 );
